a2b6bda4bf3bde7d29782155156818609c6a6b73
Arik <alikdv@gmail.com>
2018-11-02 00:06:46 -0400

    comit for upload

---
+++ Text Editor vb/Assignment3_AlexeiDvorezki.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Assignment3_AlexeiDvorezki
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.tbOpen = New System.Windows.Forms.Button()
+        Me.tbSave = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SizeBox = New System.Windows.Forms.ComboBox()
+        Me.nMredo = New System.Windows.Forms.Button()
+        Me.nMUndo = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SaveWork = New System.Windows.Forms.SaveFileDialog()
+        Me.openWork = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(1255, 637)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.TextBox1.Location = New System.Drawing.Point(114, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(85, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(12, 16)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.RadioButton1.Location = New System.Drawing.Point(940, 20)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(90, 21)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.Text = "SansSerif"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.RadioButton2.Location = New System.Drawing.Point(1047, 20)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(102, 21)
+        Me.RadioButton2.TabIndex = 4
+        Me.RadioButton2.Text = "Monospace"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.RadioButton3.Location = New System.Drawing.Point(1166, 20)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(57, 21)
+        Me.RadioButton3.TabIndex = 5
+        Me.RadioButton3.Text = "Arial"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.CheckBox1.Location = New System.Drawing.Point(613, 20)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(91, 21)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Underline"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.CheckBox2.Location = New System.Drawing.Point(710, 20)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(58, 21)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "Italic"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.CheckBox3.Location = New System.Drawing.Point(774, 20)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(58, 21)
+        Me.CheckBox3.TabIndex = 8
+        Me.CheckBox3.Text = "Bold"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.tbOpen)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.tbSave)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SizeBox)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.nMredo)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.nMUndo)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox2)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.CheckBox3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.RadioButton3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.RadioButton1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.RadioButton2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.RichTextBox1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(1255, 695)
+        Me.SplitContainer1.SplitterDistance = 54
+        Me.SplitContainer1.TabIndex = 9
+        '
+        'tbOpen
+        '
+        Me.tbOpen.ImageKey = "(none)"
+        Me.tbOpen.Location = New System.Drawing.Point(215, 16)
+        Me.tbOpen.Name = "tbOpen"
+        Me.tbOpen.Size = New System.Drawing.Size(51, 29)
+        Me.tbOpen.TabIndex = 18
+        Me.tbOpen.Text = "&Open"
+        Me.tbOpen.UseVisualStyleBackColor = True
+        '
+        'tbSave
+        '
+        Me.tbSave.ImageKey = "(none)"
+        Me.tbSave.Location = New System.Drawing.Point(272, 16)
+        Me.tbSave.Name = "tbSave"
+        Me.tbSave.Size = New System.Drawing.Size(51, 29)
+        Me.tbSave.TabIndex = 17
+        Me.tbSave.Text = "&Save"
+        Me.tbSave.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(329, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 17)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Size"
+        '
+        'SizeBox
+        '
+        Me.SizeBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SizeBox.FormattingEnabled = True
+        Me.SizeBox.Items.AddRange(New Object() {"8px ", "10px "})
+        Me.SizeBox.Location = New System.Drawing.Point(370, 18)
+        Me.SizeBox.Name = "SizeBox"
+        Me.SizeBox.Size = New System.Drawing.Size(55, 24)
+        Me.SizeBox.TabIndex = 15
+        '
+        'nMredo
+        '
+        Me.nMredo.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nMredo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.nMredo.Location = New System.Drawing.Point(431, 16)
+        Me.nMredo.Name = "nMredo"
+        Me.nMredo.Size = New System.Drawing.Size(85, 29)
+        Me.nMredo.TabIndex = 14
+        Me.nMredo.Text = "&Redo"
+        Me.nMredo.UseVisualStyleBackColor = True
+        '
+        'nMUndo
+        '
+        Me.nMUndo.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nMUndo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.nMUndo.Location = New System.Drawing.Point(522, 16)
+        Me.nMUndo.Name = "nMUndo"
+        Me.nMUndo.Size = New System.Drawing.Size(85, 29)
+        Me.nMUndo.TabIndex = 13
+        Me.nMUndo.Text = "&Undo"
+        Me.nMUndo.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Location = New System.Drawing.Point(838, 16)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(85, 29)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Reset"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SaveWork
+        '
+        '
+        'openWork
+        '
+        Me.openWork.FileName = "openWork"
+        '
+        'Assignment3_AlexeiDvorezki
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1255, 695)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Assignment3_AlexeiDvorezki"
+        Me.Text = "Text editor"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents nMredo As Button
+    Friend WithEvents nMUndo As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents SizeBox As ComboBox
+    Friend WithEvents tbSave As Button
+    Friend WithEvents SaveWork As SaveFileDialog
+    Friend WithEvents tbOpen As Button
+    Friend WithEvents openWork As OpenFileDialog
+End Class
---
+++ Text Editor vb/Assignment3_AlexeiDvorezki.vb
@@ -0,0 +1,190 @@
+﻿'Project Text Editor
+'Copyright(C) 2018 Alexei Dvoreki
+
+'This program Is free software: you can redistribute it And / Or modify
+'it under the terms Of the GNU General Public License As published by
+'the Free Software Foundation, either version 3 Of the License, Or
+'any later version.
+
+'This program Is distributed In the hope that it will be useful,
+'  but WITHOUT ANY WARRANTY; without even the implied warranty Of
+'MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.See the
+'GNU General Public License For more details.
+
+'  See <https://www.gnu.org/licenses/> to receive a copy of the GNU General Public License.
+
+Imports System.Drawing.Text
+
+Public Class Assignment3_AlexeiDvorezki
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateComboSize()
+
+    End Sub
+
+
+    Private Sub PopulateComboSize()
+        Dim i As Integer
+        For i = 8 To 60 Step 4
+            SizeBox.Items.Add(i)
+        Next i
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim startText As Integer = 0
+        Dim endText As Integer
+
+        endText = RichTextBox1.Text.LastIndexOf(TextBox1.Text)
+
+        RichTextBox1.SelectAll()
+        RichTextBox1.SelectionBackColor = Color.White
+
+        While startText < endText
+
+            RichTextBox1.Find(TextBox1.Text, startText, RichTextBox1.TextLength, RichTextBoxFinds.MatchCase)
+            RichTextBox1.SelectionBackColor = Color.Pink
+
+            startText = RichTextBox1.Text.IndexOf(TextBox1.Text, startText) + 1
+        End While
+
+    End Sub
+
+    Private Function GetDefaultFont() As Font
+        Return New Font("Sans Serif", 10, FontStyle.Regular)
+    End Function
+    'OPEN 
+    Private Sub Open()
+        If openWorks.ShowDialog = Windows.Forms.DialogResult.OK Then
+
+            RichTextBox1.LoadFile(openWorks.FileName,
+                              RichTextBoxStreamType.PlainText)
+
+        End If
+    End Sub
+    'SAVE 
+    Private Sub Save()
+        If SaveWork.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Try
+                RichTextBox1.SaveFile(SaveWork.FileName,
+                                  RichTextBoxStreamType.PlainText)
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        'check box to change text for Arial 
+
+        RichTextBox1.Font = New Font("Arial", 12.0F)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub  CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        ' bold
+
+
+        RichTextBox1.Font = New Font(RichTextBox1.Font.FontFamily, RichTextBox1.Font.Size, FontStyle.Bold)
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        '  'font style change to  Italic
+
+        RichTextBox1.Font = New Font(RichTextBox1.Font.FontFamily, RichTextBox1.Font.Size, FontStyle.Italic)
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        ''font style change to  Underline 
+
+        RichTextBox1.Font = New Font(RichTextBox1.Font.FontFamily, RichTextBox1.Font.Size, FontStyle.Underline)
+
+    End Sub
+
+    Private Sub SplitContainer1_Panel2_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel2.Paint
+
+    End Sub
+
+    Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs) Handles SplitContainer1.Panel1.Paint
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        'check box to change text for Sanserif 
+        RichTextBox1.Font = New Font("Sanserif", 12.0F)
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        'check box to change text for GenericMonospace 
+
+        RichTextBox1.Font = New Font(FontFamily.GenericMonospace, 12.0F)
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles nMUndo.Click
+        RichTextBox1.Undo()
+    End Sub
+
+    Private Sub nMredo_Click(sender As Object, e As EventArgs) Handles nMredo.Click
+        RichTextBox1.Redo()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'Reset
+        RichTextBox1.Font = New Drawing.Font("Times New Roman", 12.0F)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles SizeBox.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub SizeBox_SelectedValueChanged(sender As Object, e As EventArgs) Handles SizeBox.SelectedValueChanged
+        Dim Size As Single = Trim(Mid(SizeBox.Text, 1, 2))
+        RichTextBox1.Font = New System.Drawing.Font("Calibri", Size, FontStyle.Regular)
+    End Sub
+
+    Private Sub SplitContainer1_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles SplitContainer1.SplitterMoved
+
+    End Sub
+    'Save 
+    Private Sub tbSave_Click(sender As Object, e As EventArgs) Handles tbSave.Click
+        Save()
+    End Sub
+
+    Private Sub SaveFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveWork.FileOk
+
+    End Sub
+
+    Private Sub tbOpen_Click(sender As Object, e As EventArgs) Handles tbOpen.Click
+        Open()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        AboutBox1.ShowDialog()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        AboutBox3.ShowDialog()
+    End Sub
+
+    Private Sub Button5_Click_1(sender As Object, e As EventArgs)
+        AboutBox4.ShowDialog()
+
+    End Sub
+End Class
